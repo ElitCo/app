a674e51ce12ef483c23a9741b10c08c8bc2475ed
Pouya <pouyababaie123@gmail.com>
2024-03-15 04:44:37 +0330

    [FIX] improved layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: AppComponent,
+        pathMatch: 'full'
+    },
+    {
+        path: 'auth',
+        loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+    }
+];
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,12 +1,37 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+
+  {
+    path:'auth',
+    children:[
+
+      {
+        path:'',
+        redirectTo:'login'
+      },
+
+      {
+        path: 'login',
+        loadComponent: () => import('./login/login.component').then((c) => c.LoginComponent)
+      },
 
 
+    ]
+  },
+
+
+  
+
+]
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes)
   ]
 })
 export class AuthModule { }
